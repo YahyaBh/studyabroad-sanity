7688aef97f90b8dbe2652ed4f55a4740c551a370
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-02-16 00:48:44 +0100

    Update index.js

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import consultation from "./schemas/consultation";
+import contact from "./schemas/contact";
+import home from "./schemas/home";
+import services from "./schemas/services";
+import university from "./schemas/university";
+
+export const schemaTypes = [consultation, contact, home, services, university]
